42570fcc3caa7d0419c9c4caa54a4e80a5fd8fd8
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-22 09:47:19 +0530

    Learned to write and render multiple components in the same file

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css';
 
+//Title component
+function Title(){
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+//Description component
+function Description(){
+  return (
+    <h2>This is a Description</h2>
+  );
+}
 function App() {
-  return <h1>Mohd Mudassir Khan</h1>;
+  return (
+    <div>
+      <h1>Mohd Mudassir Khan</h1>
+      <Title/>
+      <Description/>
+    </div>
+  );
 }
 
 export default App
