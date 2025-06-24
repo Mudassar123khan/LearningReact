b1f01cd742b4c5def8b3970eb71621df83810e03
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-24 12:06:57 +0530

    how to add elements on the basis of some condition.

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,11 +1,23 @@
 import "./Product.css"
-function Product({title,features}){
+function Product({title,features,price}){
     const list=features.map((feature=> <li>{feature}</li>));
-    return (
+    if(price>=40000){
+         return (
         <div className="Product">
         <h2>{title}</h2>
+        <h4>Price: {price}</h4>
+        <p>{list}</p>
+        <p>You got 5% of discount on {title}</p>
+        </div>
+    );
+    }else{
+         return (
+        <div className="Product">
+        <h2>{title}</h2>
+        <h4>Price: {price}</h4>
         <p>{list}</p>
         </div>
     );
+    }
 }
 export default Product;
--- react-app/src/ProductTab.jsx
+++ react-app/src/ProductTab.jsx
@@ -5,9 +5,9 @@ function ProductTab(){
 
     return (
         <>
-        <Product title="Phone" features={features}/>
-        <Product title="PC" features={["CPU","GPU"]} />
-        <Product title="Laptop" features={["intel core 7","NVIDIA RTX 3050 graphics card"]}/>
+        <Product title="Phone" features={features} price={30000}/>
+        <Product title="PC" features={["CPU","GPU"]} price={100000}/>
+        <Product title="Laptop" features={["intel core 7","NVIDIA RTX 3050 graphics card"]} price={70000}/>
         </>
     );
 }
