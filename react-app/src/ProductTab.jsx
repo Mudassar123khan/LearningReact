0d3d531619ca52fa0dbb19fb27b28b3de747ffc1
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-24 11:40:30 +0530

    Array rendering. This is how we render the individual values of the array in list form.

--- react-app/src/ProductTab.jsx
+++ react-app/src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from "./Product.jsx";
 
 function ProductTab(){
+    let features=[<li>"Oled display"</li>,<li>high performance</li>];
+
     return (
         <>
-        <Product title="Phone" features={[" oled display"," high performance"]}/>
+        <Product title="Phone" features={features}/>
         <Product title="PC"  />
-        <Product title="Laptop" features={["intel core 7",", NVIDIA RTX 3050 graphics card"]}/>
+        <Product title="Laptop" features={[<li>"intel core 7"</li>,<li>"NVIDIA RTX 3050 graphics card"</li>]}/>
         </>
     );
 }
