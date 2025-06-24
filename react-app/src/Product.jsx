57e3a94d0993bcd3231abea07bbb4586c83d1d59
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-24 12:23:56 +0530

    how to add style in the same jsx file

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,9 +1,9 @@
 import "./Product.css"
 function Product({title,features,price}){
     const list=features.map((feature=> <li>{feature}</li>));
-
+    let styles={backgroundColor:"white"};
          return (
-        <div className="Product">
+        <div className="Product" style={styles}>
         <h2>{title}</h2>
         <h4>Price: {price}</h4>
         <p>{list}</p>
