9613f12357f5a08ef77b509d3b33d3e0e13afe0b
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-24 12:18:40 +0530

    This method is more efficient becaues it does not create any extra node in the DOM

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,14 +1,13 @@
 import "./Product.css"
 function Product({title,features,price}){
     const list=features.map((feature=> <li>{feature}</li>));
-    let isDiscount=price>30000 ? "Discount:5%" : "";
-    
+
          return (
         <div className="Product">
         <h2>{title}</h2>
         <h4>Price: {price}</h4>
         <p>{list}</p>
-        <p>{isDiscount}</p>
+        {price>30000 ? <p>"Discount:5%"</p> : null}
         </div>
     );
 }
