d0e163378c9864f965c93be2b470447003c93b18
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-24 12:14:46 +0530

    This method of conditions is more clean than the privious one but it creates extra node in DOM, which is inefficient.

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,23 +1,15 @@
 import "./Product.css"
 function Product({title,features,price}){
     const list=features.map((feature=> <li>{feature}</li>));
-    if(price>=40000){
+    let isDiscount=price>30000 ? "Discount:5%" : "";
+    
          return (
         <div className="Product">
         <h2>{title}</h2>
         <h4>Price: {price}</h4>
         <p>{list}</p>
-        <p>You got 5% of discount on {title}</p>
+        <p>{isDiscount}</p>
         </div>
     );
-    }else{
-         return (
-        <div className="Product">
-        <h2>{title}</h2>
-        <h4>Price: {price}</h4>
-        <p>{list}</p>
-        </div>
-    );
-    }
 }
 export default Product;
