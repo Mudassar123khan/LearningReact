37b72145b30f23c4ac2b8790139d84465bb996d5
Mudassir Khan <12mudassarkhan@gmail.com>
2025-06-24 10:59:36 +0530

    added some styling in the product cards

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,9 +1,10 @@
+import "./Product.css"
 function Product(){
     return (
-        <>
+        <div className="Product">
         <h2>Product title</h2>
         <p>Product Description</p>
-        </>
+        </div>
     );
 }
 export default Product;
