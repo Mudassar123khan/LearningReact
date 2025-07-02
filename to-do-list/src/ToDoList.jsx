9cbb2dd4fdcd6a8d3c8b57bc3526f4bcc0d04231
Mudassir Khan <12mudassarkhan@gmail.com>
2025-07-02 13:58:32 +0530

    the basic todo list is ready

---
+++ to-do-list/src/ToDoList.jsx
@@ -0,0 +1,87 @@
+import {useState} from "react"
+import { v4 as uuidv4 } from 'uuid';
+
+export default function ToDoList(){
+    let [todos,setTodos]=useState([{task:"CP",id:uuidv4(),isDone:false}]);
+    let [newTask,setNewTask]=useState("");
+    function addNewTask(){
+        setTodos((prevTodos)=>{
+            return [...prevTodos,{task:newTask,id:uuidv4(),isDone:false}]
+        });
+        setNewTask("");
+    }
+
+    function updateTodoValue(event){
+        setNewTask(event.target.value);
+    }
+
+    function deleteTask(id){
+        let newArr=todos.filter((todo)=>( todo.id!=id));
+        console.log(newArr);
+        setTodos(newArr);
+    }
+
+    function updateTasks(){
+      let newarr=   todos.map((todo)=>{
+            return {
+                ...todo,
+                task:todo.task.toUpperCase()
+            };
+        });
+        setTodos(newarr);
+    }
+
+    function markDone(id){
+        let newMark=todos.map((todo)=>{
+            if(todo.id==id){
+                return {
+                    ...todo,
+                    isDone:!todo.isDone,
+                 
+                }
+            }else{
+                return todo;
+            }
+    });
+    setTodos(newMark);
+    }
+
+    return (
+        <div>
+           
+           <input 
+           placeholder="enter your task" 
+           type="text" 
+           style={{padding:"12px",borderRadius:"10px"}} 
+           value={newTask} 
+           onChange={updateTodoValue}>
+           </input>
+
+           <button onClick={addNewTask}>Add</button>
+           <br></br>
+           <br></br>
+           <hr></hr>
+            <h4>To do list</h4>
+            <ul>
+                {
+                    todos.map((todo)=>(
+                        <li key={todo.id}>
+                         <span style={todo.isDone? {textDecorationLine:"line-through"}:{}}> {todo.task} &nbsp;&nbsp; 
+                            <button onClick={()=>{markDone(todo.id)}}>{todo.isDone? (<i className="fa-solid fa-square-check"></i>):(<i className="fa-regular fa-square-check"></i>)}</button>
+                            <button onClick={()=>{deleteTask(todo.id)}}>Delete</button>
+
+                            </span>  
+                            </li>
+                    ))
+                    
+                }
+            </ul>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <button onClick={updateTasks}>Update</button>
+       
+        </div>
+    );
+}
