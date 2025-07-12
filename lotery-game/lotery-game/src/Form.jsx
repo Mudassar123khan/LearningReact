201f00000db804c50fd5e057e0f14d0ec347fbda
Mudassir Khan <12mudassarkhan@gmail.com>
2025-07-12 10:53:57 +0530

    Learnt how to create multiple input element in a form

--- lotery-game/lotery-game/src/Form.jsx
+++ lotery-game/lotery-game/src/Form.jsx
@@ -1,22 +1,63 @@
 import { useState } from "react";
 
-export default function Form(){
-    let [fullName,setFullName] = useState("");
-    function handle(event){
-        console.log(event);
-        setFullName(event.target.value);
-    }
-    function prevent(event){
-        event.preventDefault();
-        setFullName("");
-    }
-    return (
-        <>
-        <form onSubmit={prevent}>
-            <label htmlFor="username">Name</label>
-            <input type="text" placeholder="enter some value" value={fullName} onChange={handle} id="username"></input>
+export default function Form() {
+  let [formData, setFormData] = useState({
+    fullName: "",
+    userName: "",
+    password:""
+  });
+
+  function handleChange(event) {
+    setFormData((currData)=>{
+        return {...currData,
+            [event.target.name]:event.target.value
+        };
+    });
+  };
+
+  function prevent(event) {
+    event.preventDefault();
+    setFormData({
+            fullName:"",
+            userName:"",
+            password:""
+        }
+    )
+  }
+  return (
+    <>
+      <form onSubmit={prevent} id="userNameForm">
+        <label htmlFor="username">userName</label>
+        <input
+          type="text"
+          placeholder="enter username"
+          value={formData.userName}
+          onChange={handleChange}
+          name="userName"
+          id="userName"
+        ></input>
+     
+        <label htmlFor="fullName">fullName</label>
+        <input
+          type="text"
+          placeholder="enter full name"
+          value={formData.fullName}
+          onChange={handleChange}
+          name="fullName"
+          id="fullName"
+        ></input>
+        
+        <label htmlFor="password">password</label>
+        <input 
+            type="password"
+            placeholder="enter your password"
+            onChange={handleChange}
+            value={formData.password}
+            name="password"
+            id="password"
+            ></input>
             <button>Submit</button>
-        </form>
-        </>
-    );
+         </form>
+    </>
+  );
 }
